fee9420d792688575bb536890742e7d9e615ec81
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-06 11:09:05 -0700

    updates station spec

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -8,17 +8,23 @@ describe Station, type: :model do
 
   it 'can have attributes' do
    attributes = {
-     name: "polly pocket station",
-     address: "123 Main st.",
-     fuel_type: "ELEC",
+     station_name: "polly pocket station",
+     street_address: "123 Main st",
+     city: "Denver",
+     state: "CO",
+     zip: "80203",
+     fuel_type_code: "ELEC",
      distance: "2.3333",
      access_days_time: "24 hours daily"
    }
 
    station_facade = Station.new(attributes)
-   expect(station_facade.name).to eq(attributes[:name])
-   expect(station_facade.address).to eq(attributes[:address])
-   expect(station_facade.fuel_type).to eq(attributes[:fuel_type])
+   expect(station_facade.station_name).to eq(attributes[:station_name])
+   expect(station_facade.city).to eq(attributes[:city])
+   expect(station_facade.state).to eq(attributes[:state])
+   expect(station_facade.zip).to eq(attributes[:zip])
+   expect(station_facade.street_address).to eq(attributes[:street_address])
+   expect(station_facade.fuel_type_code).to eq(attributes[:fuel_type_code])
    expect(station_facade.distance).to eq(attributes[:distance])
    expect(station_facade.access_days_time).to eq(attributes[:access_days_time])
   end
